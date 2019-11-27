a5c1e17777f3db40d9e625aa6eb076b9058f5362
Jakub Kaláb <jakubkalab@gmail.com>
2019-11-27 13:50:23 +0100

    used rspec predicate matchers

--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe do # rubocop:disable Metrics/BlockLength
     it 'works' do
       get '/'
 
-      expect(last_response.ok?).to be true
+      expect(last_response).to be_ok
       expect(last_response.body).to include 'tady bude seznam rout'
     end
   end
@@ -14,30 +14,30 @@ RSpec.describe do # rubocop:disable Metrics/BlockLength
     it 'creates, lists and returns games' do
       get '/games'
 
-      expect(last_response.ok?).to be true
+      expect(last_response).to be_ok
       expect(last_response.json['ids']).to be_empty
 
       post '/games'
 
-      expect(last_response.created?).to be true
+      expect(last_response).to be_created
 
       id = last_response.json['id']
 
       get '/games'
 
-      expect(last_response.ok?).to be true
+      expect(last_response).to be_ok
       expect(last_response.json['ids']).to include id
 
       get "/games/#{id}"
 
-      expect(last_response.ok?).to be true
+      expect(last_response).to be_ok
       expect(last_response.json['id']).to eq id
       expect(last_response.json['grid']).to have(9).rows
       expect(last_response.json['grid']).to all(have(9).columns)
 
       get '/games/666'
 
-      expect(last_response.not_found?).to be true
+      expect(last_response).to be_not_found
     end
   end
 end
